e449b3ffbce8a3c188f74038598682987ee8cb56
Gerardo Torres <gtc250@nyu.edu>
2018-09-14 23:00:04 -0400

    Add rec02.cpp

--- labs/lab02/rec02.cpp
+++ labs/lab02/rec02.cpp
@@ -2,8 +2,10 @@
 Author: Gerardo Torres
 Class: CS-UY 2124
 File Name: rec02.cpp
-Description: takes a text file of Hydrocarbons and sorts them into a 
-table based on their chemical formulas
+
+Description: takes a text file of Hydrocarbons and sorts 
+them into a table based on their Carbon count
+
 compile: `$ g++ rec02.cpp -o rec02`
 run: `$ ./rec02`
 */
@@ -17,74 +19,115 @@ run: `$ ./rec02`
 using namespace std;
 
 struct Hydrocarbon {
-    string chemicalName;
-    vector<string> vectorOfNames;
     int numberOfC;
     int numberOfH;
+    vector<string> vNames;
 };
 
-// returns
-int findFormulaIndex(int numOfC, int numOfH, vector<Hydrocarbon> &vHydrocarbs)
-{
-    for (size_t i; i < vHydrocarbs.size(); ++i) {
-        cout << vHydrocarbs[i].numberOfC << endl;
-        int numC = vHydrocarbs[i].numberOfC;
-        int numH = vHydrocarbs[i].numberOfH;
-        if ((numC == numOfC) && (numH == numOfH))
-        {
-            vHydrocarbs[i].vectorOfNames.push_back(currFormula.chemicalName);
+// returns whether hydrocarbon exists in the vector
+bool hcExists(vector<Hydrocarbon> &vHydroCarbs, int numOfC, int numOfH) {
+    int length = vHydroCarbs.size();
+    for (size_t i = 0; i < length; ++i) {
+        Hydrocarbon currHC = vHydroCarbs[i];
+        if ((currHC.numberOfC == numOfC) && (currHC.numberOfH == numOfH)) {
+            return true;
         }
     }
+    return false;
+}
 
+// returns index of where to place
+int findHydroCarbIndex(vector<Hydrocarbon> &vHydroCarbs, int numOfC, int numOfH) {
+    int length = vHydroCarbs.size();
+    for (size_t i = 0; i < length; ++ i) {
+        Hydrocarbon currHC = vHydroCarbs[i];
+        if ((currHC.numberOfC == numOfC) && (currHC.numberOfH == numOfH)) {
+            return i;
+        }
+    }
 }
 
 // takes the stream and inserts forumlas into vector as Formula structures
-void readIntoVector(ifstream &ifs, vector<Hydrocarbon> &vHydrocarbs)
-{
-    Hydrocarbon currFormula;
+void readIntoVector(ifstream &ifs, vector<Hydrocarbon> &vHydroCarbs) {
     char carbon, hydrogen;
     int numOfC, numOfH;
-    while (ifs >> currFormula.chemicalName >> carbon >> numOfC >> hydrogen >> numOfH) {
-        findFormulaIndex(numOfC, numOfH, vHydrocarbs);
-    }
-    for (size_t i; i < vHydrocarbs.size(); ++i) {
-        cout << vHydrocarbs[i].chemicalName << " c" << vHydrocarbs[i].numberOfC 
-        << " h" << vHydrocarbs[i].numberOfH << endl;
+    string name;
+    while (ifs >> name >> carbon >> numOfC >> hydrogen >> numOfH) {
+        Hydrocarbon currHydroCarb;
+        if (hcExists(vHydroCarbs, numOfC, numOfH)) {
+            // current hydrocarbon config exists; find at which index to insert
+            int insertIndex = findHydroCarbIndex(vHydroCarbs, numOfC, numOfH);
+            Hydrocarbon indexedHydroCarb = vHydroCarbs[insertIndex];
+            vHydroCarbs[insertIndex].vNames.push_back(name);
+        } else {
+            // current hydrocarbon config does not exist; create a new one
+            currHydroCarb.numberOfC = numOfC;
+            currHydroCarb.numberOfH = numOfH;
+            currHydroCarb.vNames.push_back(name);
+            vHydroCarbs.push_back(currHydroCarb);
+        }
     }
 }
 
 // asks user for filename and returns ifstream object ifs
-ifstream askForFilename() {
+void askForFilename(ifstream &ifs) {
     string fileName;
-    cout << "Please enter a file name" << endl;
+    cout << "Please enter a file name\n";
     cin >> fileName;
-    ifstream ifs(fileName);
+    ifs.open(fileName);
     while (!ifs) {
-        cout << "Filename is invalid please try again" << endl;
-        cin >> fileName;
         ifs.clear();
+        cout << "Filename is invalid please try again\n";
+        cin >> fileName;
         ifs.open(fileName);
     }
-    return ifs;
 }
 
-// bubblesort implementation to sort vector of strings in-place
-void bubbleSort(vector<string> vectorOfNames) {
-    int i;
-    int j;
-    int length = vectorOfNames.size();
-    for (i = 0; i < length - 1; i++) {
-        for (j = 0; length - i - 1; j++) {
-            if (vectorOfNames[j] > vectorOfNames[j + 1]) {
-                // vectorOfNames[j], vectorOfNames[j + 1] = vectorOfNames[j + 1], vectorOfNames[j];
-                // swap(&vectorOfNames[j], &vectorOfNames[j + 1]);
+void swap(vector<Hydrocarbon> &vHydroCarbs, int j) {
+    Hydrocarbon temp = vHydroCarbs[j];
+    vHydroCarbs[j] = vHydroCarbs[j + 1];
+    vHydroCarbs[j + 1] = temp;
+}
+
+// bubble sort implementation to sort hydrocarbons by carbon count in-place
+void bubbleSort(vector<Hydrocarbon> &vHydroCarbs) {
+    int i, j;
+    int length = vHydroCarbs.size();
+    // iterating through Hydrocarbon
+    for (size_t i = 0; i < length; i++) {
+        for (size_t j = 0; j < length - i - 1; j++) {
+            if (vHydroCarbs[j].numberOfC > vHydroCarbs[j + 1].numberOfC) {
+                // sorting by Carbons
+                swap(vHydroCarbs, j);
+            } else if (vHydroCarbs[j].numberOfC == vHydroCarbs[j + 1].numberOfC) {
+                // if Carbons are equal, Hydrogen is the tiebreaker
+                if (vHydroCarbs[j].numberOfH > vHydroCarbs[j + 1].numberOfH) {
+                    swap(vHydroCarbs, j);
+                }
             }
         }
     }
 }
 
+// display hydroarbons to the terminal
+void displayHydrocarbons(vector<Hydrocarbon> &vHydroCarbs) {
+    int lengthVector = vHydroCarbs.size();
+    for (size_t i = 0; i < lengthVector; ++i) {
+        Hydrocarbon currHC = vHydroCarbs[i];
+        cout << 'C' << currHC.numberOfC << 'H' << currHC.numberOfH << " -> ";
+        int lengthNames = currHC.vNames.size();
+        for (size_t j = 0; j < lengthNames; ++j) {
+            cout << currHC.vNames[j] << ", ";
+        }
+        cout << '\n'; // end of hydrocarbon object
+    }
+}
+
 int main() {
     vector<Hydrocarbon> vHydroCarbs;
-    ifstream ifs = askForFilename();
+    ifstream ifs;
+    askForFilename(ifs);
     readIntoVector(ifs, vHydroCarbs);
+    bubbleSort(vHydroCarbs);
+    displayHydrocarbons(vHydroCarbs);
 }
